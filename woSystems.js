3c393a4d76dddc2627f3e44c88b1dd107add79cf
Gareth Leak <gleaks@gmail.com>
2020-07-01 16:51:09 -0600

    refactoring, comments, extra node modules

--- woSystems.js
+++ woSystems.js
@@ -29,47 +29,23 @@ base('Work Orders').select({
     const rs = record.get('Requirement String');
     // If the string contains AC, DC or IC we know it needs a testhead
     if (rs.includes('AC') || rs.includes('DC') || rs.includes('IC')) {
-      // Fill in the rest of the system object then push it into our array
+      // makeSystem creates a properly formatted System object, we push it into the array
       systems.push(dict.makeSystem('Testhead', 'A', record.id));
-      console.log(systems);
     }
-    // If the string contains CAL it needs a cal box
-    if (rs.includes('CAL')) {
-      systems.push(dict.makeSystem('CAL', 'C', record.id));
-      console.log(systems);
-    }
-    if (rs.includes('DRDSON')) {
-      systems.push(dict.makeSystem('DRDSON', 'DRD', record.id));
-    }
-    if (rs.includes('DXM')) {
-      systems.push(dict.makeSystem('DXM', 'D', record.id));
-    }
-    if (rs.includes('HTS')) {
-      systems.push(dict.makeSystem('HTS', 'H', record.id));
-    }
-    if (rs.includes('IND')) {
-      systems.push(dict.makeSystem('Inductor', 'IND', record.id));
-    }
-    if (rs.includes('MDM')) {
-      systems.push(dict.makeSystem('MDM', 'M', record.id));
-    }
-    // QG CAL
-    if (rs.includes('QC')) {
-      systems.push(dict.makeSystem('QG-CAL', 'Q', record.id));
-    }
-    // RG CAL
-    if (rs.includes('CG')) {
-      systems.push(dict.makeSystem('RG-CAL', 'R', record.id));
-    }
-    // UHV
-    if (rs.includes('ULTRA')) {
-      systems.push(dict.makeSystem('UHV', 'U', record.id));
-    }
+    if (rs.includes('CAL')) systems.push(dict.makeSystem('CAL', 'C', record.id));
+    if (rs.includes('DRDSON')) systems.push(dict.makeSystem('DRDSON', 'DRD', record.id));
+    if (rs.includes('DXM')) systems.push(dict.makeSystem('DXM', 'D', record.id));
+    if (rs.includes('HTS')) systems.push(dict.makeSystem('HTS', 'H', record.id));
+    if (rs.includes('IND')) systems.push(dict.makeSystem('Inductor', 'IND', record.id));
+    if (rs.includes('MDM')) systems.push(dict.makeSystem('MDM', 'M', record.id));
+    if (rs.includes('QC')) systems.push(dict.makeSystem('QG-CAL', 'Q', record.id));
+    if (rs.includes('CG')) systems.push(dict.makeSystem('RG-CAL', 'R', record.id));
+    if (rs.includes('ULTRA')) systems.push(dict.makeSystem('UHV', 'U', record.id));
 
     console.log(`Modified ${record.get('Name')} (${record.id})`);
     console.log(systems);
 
-    // Now its time to build some systems
+    // We give the entire array of systems to the Airtable API and tell it to create records
     base('System Log').create(systems, (error) => {
       if (error) {
         console.error(error);
